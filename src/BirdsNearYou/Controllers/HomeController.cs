6ea5b6a8fbe7a394fb9b60d99496c8eb59617eea
Liam Dyer <ldyer2@alaska.edu>
2024-06-25 08:20:01 -0700

    Updated embeded power bi data

--- src/BirdsNearYou/Controllers/HomeController.cs
+++ src/BirdsNearYou/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace BirdsNearYou.Controllers
             return View();
         }
 
+        public IActionResult BirdSightingsData()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
